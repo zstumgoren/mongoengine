3ec0144270f32acf5c3fa46dfa9e4c44b1baf311
Ross Lawley <ross.lawley@gmail.com>
2011-11-30 06:34:10 -0800

    Updated to handle null data from the database

--- mongoengine/base.py
+++ mongoengine/base.py
@@ -754,12 +754,10 @@ class BaseDocument(object):
 
         # Handle None values for required fields
         if value is None and name in getattr(self, '_fields', {}):
-            field = self._fields[name]
-            if not field.required:
-                self._data[name] = value
-                if hasattr(self, '_changed_fields'):
-                    self._mark_as_changed(name)
-                return
+            self._data[name] = value
+            if hasattr(self, '_changed_fields'):
+                self._mark_as_changed(name)
+            return
         super(BaseDocument, self).__setattr__(name, value)
 
     def __expand_dynamic_values(self, name, value):
