ad471a51f3ce7a7cd9b1f288b97cfec33a9b5e6a
Serdar Tumgoren <tumgorenz@washpost.com>
2012-07-10 22:27:30 -0400

    add MongoTestRunner that works for both relational and Mongo backends

--- mongoengine/django/tests.py
+++ mongoengine/django/tests.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 from django.conf import settings
-from django.test import TestCase, TransactionTestCase
+from django.test import TestCase
 from django.test.simple import DjangoTestSuiteRunner
 
 from mongoengine import connect
@@ -23,53 +23,42 @@ class MongoTestCase(TestCase):
                 continue
             self.db.drop_collection(collection)
 
-class MongoTestCase2(TransactionTestCase):
-    """TestCase for standalone MongoDB backend that rebuilds db for each test method.
+class MongoTestRunner(DjangoTestSuiteRunner):
+    """Test runner for Mongo and, optionally, a RDBMS backend.
+
+    In addition to invoking standard Django test db creation for relational backends,
+    this test suite runner creates a test Mongo database.
+
+    Inspired by `django-mongorunner <https://github.com/xintron/django-mongorunner>`_
+
+    USAGE
+
+    In settings.py, configure the following variables:
 
         MONGO_DATABASE_NAME = 'my-mongo-db'
         TEST_RUNNER = 'mongoengine.django.tests.MongoTestRunner'
 
-    USAGE:
-    
-        from mongoengine.django.tests import MongoTestCase2
-        from myapp.models import MyDoc
-
-        class MyTestCase(MongoTestCase2)
-
-            def test_stuff(self):
-                mydoc = MyDoc(foo='bar')
-                mydoc.save()
-                self.assertEquals(mydoc.foo, 'bar')
+    This will result in creating a temporary 'test_my-mongo-db' database, 
+    which will be torn down at the end of the test run.
 
     """
 
-    def _fixture_setup(self):
-        pass
-
-
-class MongoTestRunner(DjangoTestSuiteRunner):
-    """Test runner for running Mongo without an RDBMS
-
-    Sets up and tears down a test database for each test method.
-    Inspired by `django-mongorunner <https://github.com/xintron/django-mongorunner>`_
-
-    USAGE:
-
-    In settings.py, configure the following variables:
-
-        TEST_RUNNER = 'mongoengine.django.tests.MongoTestRunner'
-
-    """
-
+    rdbms = settings.DATABASES['default'].get('NAME','')
     test_db = 'test_%s' % settings.MONGO_DATABASE_NAME
 
     def setup_databases(self, **kwargs):
+        # If a relational db is configured, invoke standard Django RDBMS setup
+        if self.rdbms:
+            self.old_config = super(MongoTestRunner, self).setup_databases(**kwargs)
         register_connection('default', self.test_db)
-        print 'Creating test database: ' + self.test_db
+        print 'Creating test Mongo database: ' + self.test_db
         return self.test_db
 
     def teardown_databases(self, db_name, **kwargs):
+        # If a RDBMS is configured, invoke standard Django RDBMS teardown
+        if self.rdbms:
+            super(MongoTestRunner, self).teardown_databases(self.old_config, **kwargs)
         from pymongo import Connection
         conn = Connection()
-        conn.drop_database(self.db_name)
-        print 'Dropping test database: ' + self.db_name
+        conn.drop_database(db_name)
+        print 'Dropping test Mongo database: ' + db_name 
